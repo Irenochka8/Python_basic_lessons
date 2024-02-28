4f6785e42151e63f62e163bb50521baeebcfec7c
irenochka <irinazagorodnikova2@gmail.com>
2024-02-29 00:30:09 +0200

    lesson_14_2 group of students, make modules of class

---
+++ les_14_2/Group.py
@@ -0,0 +1,31 @@
+from Student import Student
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = "; ".join([str(student) for student in self.group])
+        return f'Number:{self.number}\n{all_students}'
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
